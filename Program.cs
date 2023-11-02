55f0e6c23ae6bca512f91c0e95a9ccbcb0549773
Claudivan Santos do Nascimento <claudivan040@gmail.com>
2023-11-01 23:17:57 -0300

    Desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatphone Nokia Tijolão:");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "Modelo 1", imei: "1111111111", memoria: 125 );
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smatphone Iphone 14:");
+Smartphone iphone = new Iphone(numero: "12345678", modelo: "Modelo 1", imei: "1111111111", memoria: 125 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
